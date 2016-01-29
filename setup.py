49e67a950fc14ba77d41ecc61b2aa2e6fbafa3e3
AnoshZahir <anoshzahir@hotmail.com>
2016-01-29 15:17:56 +0000

    added reference to the scripts directory

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author = "Anosh Zahir",
     author_email = "anosh.zahir15@imperial.ac.uk",
     packages = find_packages(exclude = ['*test']),
+    scripts = ['scripts/greengraph'],
     install_requires = ['argparse']
 )
 
