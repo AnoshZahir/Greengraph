679932872c435454f9aabe756637f37e09524c37
AnoshZahir <anoshzahir@hotmail.com>
2016-01-29 15:05:26 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Greengraph",
+    version = "0.1",
+    description = "Explore how green space varies between two locations.",
+    url = "https://github.com/AnoshZahir/Greengraph.git",
+    licence = "MIT",
+    author = "Anosh Zahir",
+    author_email = "anosh.zahir15@imperial.ac.uk",
+    packages = find_packages(exclude = ['*test']),
+    install_requires = ['argparse']
+)
+
+ 
