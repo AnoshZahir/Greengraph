6f932bd462da60c698b87cfa407290b82086dda4
AnoshZahir <anoshzahir@hotmail.com>
2016-02-01 16:25:19 +0000

    significant changes to test_location_sequence and test_green_between

--- Greengraph/Test/test_graph.py
+++ Greengraph/Test/test_graph.py
@@ -1,16 +1,16 @@
 from ..graph import Greengraph
 from ..map import Map
 
-import geopy
-import requests
-import numpy as np
-from matplotlib import image as img
+import geopy # not used
+import requests # not used
+import numpy as np # not used
+from matplotlib import image as img # not used
 
 import os
 import yaml
 
 from nose.tools import assert_equal, assert_almost_equal
-from mock import patch, Mock
+from mock import patch
 
 def test_Greengraph():
     '''
@@ -21,9 +21,11 @@ def test_Greengraph():
     assert_equal(actual.end, 'Cambridge')
 
 @patch.object(geopy.geocoders.GoogleV3, 'geocode')
-def test_geolocate(mock_geocoder):
+def test_geolocate(mock_geocode):
     '''
     Test that geolocate method returns the correct latitude and longitude values for given locations.
+    In the Class geopy.geocoders.GoogleV3, the method geocode is mocked.  The mocked values taken are from 
+    the test_geolocate subsection of graph_data.yaml file
     '''
     mygraph = Greengraph(0.0, 0.0)
     
@@ -34,12 +36,18 @@ def test_geolocate(mock_geocoder):
         location = data.pop('location')
         latitude = data.pop('location_lat')
         longitude = data.pop('location_long')
-        mock_geocoder.asser_equal(mygraph.geolocate(location), (latitude, longitude))
-    
+        
+        expected_return = (latitude, longitude)
+        mock_geocode.return_value = expected_return
+        
+        actual_return = mygraph.geolocate(location)
+        
+        assert_equal(actual_return, expected_return)
+
 def test_location_sequence():
     '''
-    Test that location_sequence method returns the correct values based on
-    numbers given in the method's 'start', 'end' and 'steps' arguments. 
+    Test that location_sequence method returns the correct values based on 'start', 'end' and 'steps' arguments. 
+    Data is taken from test_location_sequence subsection of graph_data.yaml.
     '''
     mygraph = Greengraph(0.0, 0.0)
     
@@ -47,41 +55,39 @@ def test_location_sequence():
         location_sequence_data = yaml.load(dataset)['test_location_sequence']
     
     for data in location_sequence_data:
-        first_location = data.pop('first_location')
-        second_location = data.pop('second_location')
+        first_location_coordinates = data.pop('first_location_coordinates')
+        second_location_coordinates = data.pop('second_location_coordinates')
         steps = data.pop('steps')
-        sequence_results = data.pop('sequence_results')
-        actual_results = mygraph.location_sequence(mygraph.geolocate(first_location), mygraph.geolocate(second_location), steps)
-        for step_location in range(0, len(actual_results)):
-            for location_coordinate in range(0, len(actual_results[step_location])):
-                assert_almost_equal(actual_results[step_location][location_coordinate], sequence_results[step_location][location_coordinate])
+        expected_return = data.pop('expected_return')
+        
+        actual_return = mygraph.location_sequence(first_location_coordinates, second_location_coordinates)
+        for step_num in range(0, steps):
+            for coordinate in range(0, 2):
+                assert_almost_equal(actual_return[step_num][coordinate], expected_return[step_num][coordinate])
 
-@patch.object(Greengraph, 'geolocate') # to account for method's dependancy on geolocate method. 
-@patch.object(Map, 'count_green') # to account for method's dependancy on 'count_green' method in 'map' class.
-@patch.object(requests, 'get') # to account for dependancy on the 'map' class where requests.get is used.
-@patch.object(img, 'imread') # to account for method's dependancy on the 'map' class: count_green() -> green() -> Map() where 'imread' is used.
-def test_green_between(mock_geolocate, mock_count_green, mock_requests_get, mock_imread):
+#decorators dont get applied top down, they get applied bottom up.
+@patch.object(Greengraph, 'location_sequence')
+@patch.object(Map, 'count_green')
+def test_green_between(mock_count_green, mock_location_sequence):
     '''
     Test that green_between returns a list of green pixels for each step between two locations.
+    mock_count_green to account for dependancy on count_green method from map class.
+    mock_location_sequence to account for dependacy on location_sequence method.
+    Data for both mocks are taken from test_green_between subsection of graph_data.yaml.
     '''
+    mygraph = Greengraph(0.0, 0.0)
     
     with open(os.path.join(os.path.dirname(__file__), 'data', 'graph_data.yaml')) as dataset:
         green_between_data = yaml.load(dataset)['test_green_between']
     
     for data in green_between_data:
-        first_location = data.pop('first_location')
-        first_location_latlong = data.pop('first_location_latlong')
-        second_location = data.pop('second_location')
-        second_location_latlong = data.pop('second_location_latlong')
+        location_sequence_values = data.pop('location_sequence_values')
+        count_green_values = data.pop('count_green_values')
         steps = data.pop('steps')
-        result = data.pop('result')
         
-        mock_geolocate.side_effect = result
-        mock_count_green.side_effect = [first_location_latlong, second_location_latlong]
+        mock_count_green.side_effect = count_green_values
+        mock_location_sequence.side_effect = location_sequence_values
         
-        green_count = Greengraph(first_location, second_location).green_between(steps)
-        assert_equal(result, green_count)
-
-        
-        
-    
+        expected_return = count_green_values
+        actual_return = mygraph.count_green(steps) 
+        assert_equal(actual_return, expected_return)
