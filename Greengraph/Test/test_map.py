c155143b34f9211b803a712a84d1dcfb50196ac1
AnoshZahir <anoshzahir@hotmail.com>
2016-02-05 14:24:42 +0000

    cleaned up comments, updated code and got rid of some typos.

--- Greengraph/Test/test_map.py
+++ Greengraph/Test/test_map.py
@@ -1,26 +1,23 @@
 from ..graph import Greengraph
 from ..map import Map
 
-import geopy
 import requests
 import numpy as np
-from matplotlib import image as img
+import matplotlib
 
 import os
 import yaml
 
-from nose.tools import assert_called_with, assert_equal, assert_almost_equal
+from nose.tools import assert_called_with, assert_equal
 from mock import patch
-import unittest
 
-def test_build_default_params():
-    '''
-    Test that the function is passing the parameters correctly to requests.get().
-    '''
-    with open(os.path.join(os.path.dirname(__file__), 'data', 'map_data.yaml')) as dataset:
-        map_data = yaml.load(dataset)['test_map']
+@patch.object(requests, 'get')
+@patch.object(matplotlib.image, 'imread')
+def test_build_default_params(mock_imread, mock_get):    
     
-    with patch.object(requests, 'get') as mock_get:
+    with open(os.path.join(os.path.dirname(__file__),'data','map_data.yaml')) as dataset:
+        map_data = yaml.load(dataset)['test_map']
+        
         for data in map_data:
             test = data.pop('test')
             url = data.pop('url')
@@ -28,37 +25,37 @@ def test_build_default_params():
             longitude = data.pop('longitude')
             params = data.pop('params')
             if (test == 'default'):
-                actual_map = Map(latitude, longitude)
+                actual_map = Map(latitude,longitude)
             elif (test == 'satellite_false'):
-                actual_map = Map(latitude, longitude, satellite = False)
+                actual_map = Map(latitude,longitude,satellite = False)
             elif (test == 'zoom'):
-                actual_map = Map(latitude, longitude, zoom = 30)
+                actual_map = Map(latitude,longitude,zoom = 30)
             elif (test == 'size'):
-                actual_map = Map(latitude, longitude, size = (300,300))
-            elif (test == 'sensor_true'):
-                actual_map = Map(latitude, longitude, sensor = 'true')
+                actual_map = Map(latitude,longitude,size = (300,300))
+            elif (test == 'sensor_true'): 
+                actual_map = Map(latitude,longitude,sensor = True)
             
-            mock_get.assert_called_with(url,params)
+            mock_get.assert_called_with(url,params=params)
 
-def test_green:
+@patch.object(requests, 'get')
+@patch(matplotlib.image, 'imread')
+#@patch.object(Map, 'green')
+def test_green(mock_imread, mock_get):
     '''
     Test that the function can take a 3d matrix, and return a matrix of true/false values.
     Data is taken from the 'test_green' subsection of map_data.yaml.
     '''
-    my_map = Map(51.50, -0.12) # create an instance of Map.
+    my_map = Map(51.50, -0.12)
     
     with open(os.path.join(os.path.dirname(__file__), 'data', 'map_data.yaml')) as dataset:
         map_data = yaml.load(dataset)['test_green']
     
     for data in map_data:
         threshold = data.pop('test')
-        3d_input_matrix = data.pop('3d_input_matrix')
+        input_matrix = data.pop('3d_input_matrix')
         expected_return = data.pop('2d_output_matrix')
-        
-        my_map.__setattr__('pixels', 3d_input_matrix)
-        
+        my_map.__setattr__('pixels', input_matrix)
         actual_return = my_map.green(threshold)
-        
         assert_equal(expected_return, actual_return)
 
 @patch.object(Map, 'green')
@@ -67,7 +64,7 @@ def test_count_green(mock_green):
     Test that a 2d input matrix of true/false values is correctly summed. 
     Data is taken from the 'test_count_green' subsection of map_data.yaml.
     '''
-    my_map = Map(51.50, -0.12) # create an instance of Map.
+    my_map = Map(51.50, -0.12)
 
     with open(os.path.join(os.path.dirname(__file__), 'data', 'map_data.yaml')) as dataset:
         map_data = yaml.load(dataset)['test_count_green']
@@ -78,6 +75,4 @@ def test_count_green(mock_green):
         
         mock_green.return_value = input_values
         actual_return = my_map.count_green() #threshold kept unchanged for this test.
-        assert.equal(actual_return, expected_return
-    
-    
+        assert_equal(actual_return, expected_return)
