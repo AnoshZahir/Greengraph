191ea1fc8cc6b63ada07dff54998497016a41d7f
AnoshZahir <anoshzahir@hotmail.com>
2016-02-05 13:57:08 +0000

    added default values for locations

--- Greengraph/command.py
+++ Greengraph/command.py
@@ -4,17 +4,17 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description = "Explore how green space varies between two locations.")
 
-parser.add_argument('-f', '--from', dest = first_location, type = str, required = True, help = 'Enter name of the first location.  This is a mandatory field and must be a string.')
-parser.add_argument('-t', '--to', dest = second_location, type = str, required = True, help = 'Enter name of the second location. This is a mandatory field and must be a string.')
-parser.add_argument('-s', '--steps' dest = steps, , type = int, default = 10, help = 'Enter the number of steps between the two locations. This is an optional integer field with default set to 10 steps.')
-parser.add_argument('-o', '--out', dest = output, default = 'output', help= 'Enter name of output file: format .png')
+parser.add_argument('-f', '--from', default = 'london', dest = 'first_location', type = str, required = False, help = 'Enter name of the first location as a string. Default set to "London".')
+parser.add_argument('-t', '--to', default = 'cambridge', dest = 'second_location', type = str, required = False, help = 'Enter name of the second location as a string. Default set to "Cambridge".')
+parser.add_argument('-s', '--steps', dest = 'steps', type = int, default = 10, help = 'Enter the number of steps between the two locations. This is an optional integer field with default set to 10 steps.')
+parser.add_argument('-o', '--out', dest = 'output', default = 'output', help= 'Enter name of output file: format .png')
     
 def green_plotter(arguments):
     this_graph = Greengraph(arguments.first_location, arguments.second_location)# create an instance of the Greengraph class object.
     green_count = this_graph.green_between(arguments.steps)
     plt.plot(green_count)
     
-    plt.title('Number of green pixels in locations between ' + arguments.first_place + ' and ' + arguments.second_place) 
+    plt.title('Number of green pixels in locations between ' + arguments.first_location + ' and ' + arguments.second_location) 
     plt.xlabel('Steps')
     plt.ylabel('Green pixels')
     
