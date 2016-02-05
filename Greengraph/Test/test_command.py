56db25af4976869d720abb95197cc11177bf383c
AnoshZahir <anoshzahir@hotmail.com>
2016-02-05 14:14:36 +0000

    cleaned up comments. got rid of code which did not work.

--- Greengraph/Test/test_command.py
+++ Greengraph/Test/test_command.py
@@ -1,9 +1,8 @@
 from ..command import parser, green_plotter, process
-from ..graph import Greengraph
-from ..map import Map
 
 from nose.tools import assert_equal, assert_true
-from mock import mock_open, patch
+
+from mock import patch
 
 def test_command_line_arguments():
     '''
@@ -15,18 +14,9 @@ def test_command_line_arguments():
     assert_equal( arguments.steps, 4)
     assert_equal( arguments.output, 'my_file')
 
-
-'''
-patch replaces MyClass in a way that allows you to control the usage of the class in functions that you call. 
-Once you patch a class, references to the class are completely replaced by the mock instance.
-
-mock.patch is usually used when you are testing something that creates a new instance of a class inside of the test.  
-mock.Mock instances are clearer and are preferred. If your self.sut.something method created an instance of MyClass 
-instead of receiving an instance as a parameter, then mock.patch would be appropriate here.
-'''
-
-@mock.patch('__main__'.Greengraph) #based on the above notes, use @mock.patch as in green_plotter method, 'this_graph' is an instance of Greengraph.
-def test_green_plotter(mock_Greengraph):
+@mock.patch('__main__'.Greengraph)
+def test_green_plotter(mock_Greengraph)
+    #update with code.
     
 @patch('command.parser.parse_args')
 @patch('command.green_plotter')
