d7690d733465a0f4c94221cbabff4b115e25215d
azazgit <anoshzahir@hotmail.com>
2024-09-17 11:56:53 +0100

    map.py: updated import StringIO to io.

--- Greengraph/map.py
+++ Greengraph/map.py
@@ -1,5 +1,5 @@
 import numpy as np
-from StringIO import StringIO
+from io import StringIO
 from matplotlib import image as img
 import requests
 
@@ -9,38 +9,38 @@ class Map(object):
         Construct a URL.
         Then assign to self.pixels the numpy array of the PNG image of the googlemaps location in the URL.
         """
-	   
+       
         base = "http://maps.googleapis.com/maps/api/staticmap?"
         
         params = dict(
-		      sensor = str(sensor).lower(),
-		      zoom = zoom, 
-		      size = "x".join(map(str, size)),
-		      center = ",".join(map(str, (lat, long))),
-		      style = "feature:all|element:labels|visibility:off"
-		      )
-	if satellite:
+              sensor = str(sensor).lower(),
+              zoom = zoom, 
+              size = "x".join(map(str, size)),
+              center = ",".join(map(str, (lat, long))),
+              style = "feature:all|element:labels|visibility:off"
+              )
+    if satellite:
             params["maptype"] = "satellite"
         
         self.image = requests.get(base, params = params).content #response.get returns a 'Response' object.
         self.pixels = img.imread(StringIO(self.image))
-	
+    
     def green(self, threshold):
         """
         Take the array to which 'self.pixels' points to.  Use the 'threshold' and return an array of True/False values depending on 
         whether each pixel is green or not. 
         """
-	greener_than_red = self.pixels[:,:,1] > threshold*self.pixels[:,:,0]
-	greener_than_blue = self.pixels[:,:,1] > threshold*self.pixels[:,:,2]
-	green = np.logical_and(greener_than_red, greener_than_blue)
+    greener_than_red = self.pixels[:,:,1] > threshold*self.pixels[:,:,0]
+    greener_than_blue = self.pixels[:,:,1] > threshold*self.pixels[:,:,2]
+    green = np.logical_and(greener_than_red, greener_than_blue)
         return green # 400x400 array of true/false values. 
-	
+    
     def count_green(self, threshold = 1.1):
         """
         Take as argument a 'threshold' and return the number of green pixels in the image.
         """
         return np.sum(self.green(threshold))
-	
+    
     def show_green(self, threshold = 1.1):
         """
         
@@ -50,4 +50,4 @@ class Map(object):
         buffer = StringIO()
         result = img.imsave(buffer, out, format = 'png')
         return buffer.getvalue()
-	
+    
