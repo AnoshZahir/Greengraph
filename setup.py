b8960398051c3e02bc854f7c4d9fd270eec19919
azazgit <anoshzahir@hotmail.com>
2024-09-17 23:34:07 +0100

    setup.py: added comments.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="Greengraph",
     version="0.1",
-    description="Explore how green space varies between two locations.",
+    description="A Python package to explore how green space varies between two locations using satellite imagery.",
     url="https://github.com/AnoshZahir/Greengraph.git",
-    licence="MIT",
+    license="MIT",
     author="Anosh Zahir",
     author_email = "", # To be added in future.
-    packages=find_packages(),  # Include all packages, including test files
-    scripts=['scripts/graph'],
+    packages=find_packages(),  # Automatically find all packages
+    scripts=['scripts/graph'], # Include the script for command-line usage
     install_requires=[
         'argparse',
         'geopy',
@@ -18,5 +18,5 @@ setup(
         'numpy',
         'mock'
     ],
-    python_requires='>=3.5'
+    python_requires='>=3.5' # Specify the required Python version
 )
