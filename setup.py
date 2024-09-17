21177f59e6d316ffb9f66fa98cecf42ef62a39f5
azazgit <anoshzahir@hotmail.com>
2024-09-17 13:54:04 +0100

    setup.py: updated file to reflect all latest changes made to the project.

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "Greengraph",
-    version = "0.1",
-    description = "Explore how green space varies between two locations.",
-    url = "https://github.com/AnoshZahir/Greengraph.git",
-    licence = "MIT",
-    author = "Anosh Zahir",
-    author_email = "anosh.zahir15@imperial.ac.uk",
-    packages = find_packages(exclude = ['*test']),
-    scripts = ['scripts/graph'],
-    install_requires = ['argparse', 'geopy', 'matplotlib', 'requests', 'numpy', 'mock' ]
+    name="Greengraph",
+    version="0.1",
+    description="Explore how green space varies between two locations.",
+    url="https://github.com/AnoshZahir/Greengraph.git",
+    licence="MIT",
+    author="Anosh Zahir",
+    author_email = "", # To be added in future.
+    packages=find_packages(),  # Include all packages, including test files
+    scripts=['scripts/graph'],
+    install_requires=[
+        'argparse',
+        'geopy',
+        'matplotlib',
+        'requests',
+        'numpy',
+        'mock',
+        'nose'  # Include nose for testing
+    ],
     python_requires='>=3.5'
 )
-
- 
