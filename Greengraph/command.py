2e2e03bd0eb75d4cebd9992f0b4582631c00354b
azazgit <anoshzahir@hotmail.com>
2024-09-17 12:37:07 +0100

    command.py: cleaned up arg parser code.

--- Greengraph/command.py
+++ Greengraph/command.py
@@ -9,12 +9,17 @@ from matplotlib import pyplot as plt
 from graph import Greengraph
 from argparse import ArgumentParser
 
+# Argument parser for the command-line interface
 parser = ArgumentParser(description = "Explore how green space varies between two locations.")
 
-parser.add_argument('-f', '--from', default = 'london', dest = 'first_location', type = str, required = False, help = 'Enter name of the first location as a string. Default set to "London".')
-parser.add_argument('-t', '--to', default = 'cambridge', dest = 'second_location', type = str, required = False, help = 'Enter name of the second location as a string. Default set to "Cambridge".')
-parser.add_argument('-s', '--steps', dest = 'steps', type = int, default = 10, help = 'Enter the number of steps between the two locations. This is an optional integer field with default set to 10 steps.')
-parser.add_argument('-o', '--out', dest = 'output', default = 'output', help= 'Enter name of output file: format .png')
+parser.add_argument('-f', '--from', default='London', dest='first_location', type=str, required=False, 
+                    help='Enter name of the first location as a string. Default set to "London".')
+parser.add_argument('-t', '--to', default='Cambridge', dest='second_location', type=str, required=False, 
+                    help='Enter name of the second location as a string. Default set to "Cambridge".')
+parser.add_argument('-s', '--steps', dest='steps', type=int, default=10, 
+                    help='Enter the number of steps between the two locations. Optional, default set to 10 steps.')
+parser.add_argument('-o', '--out', dest='output', default='output', 
+                    help='Enter the name of the output file. The file will be saved as a .png.')
     
 def green_plotter(arguments):
     this_graph = Greengraph(arguments.first_location, arguments.second_location)# create an instance of the Greengraph class object.
